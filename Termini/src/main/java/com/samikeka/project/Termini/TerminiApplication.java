bf264b5b2eae3f1ca66c2a2bbf6ada9a2edd5776
sami keka <samikeka@hotmail.com>
2025-05-04 17:27:19 +0200

    inicializimi i projektit Termini

---
+++ Termini/src/main/java/com/samikeka/project/Termini/TerminiApplication.java
@@ -0,0 +1,13 @@
+package com.samikeka.project.Termini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminiApplication.class, args);
+	}
+
+}
---
+++ Termini/src/test/java/com/samikeka/project/Termini/TerminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samikeka.project.Termini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
